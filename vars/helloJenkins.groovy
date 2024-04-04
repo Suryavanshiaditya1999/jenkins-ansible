5c519c5409b53254fb9e161887b62d5605524950
Naresh-boyini <160397679+Naresh-boyini@users.noreply.github.com>
2024-04-04 18:01:17 +0530

    Update helloJenkins.groovy

--- vars/helloJenkins.groovy
+++ vars/helloJenkins.groovy
@@ -1,4 +1,4 @@
 def call(){
 
-sh 'ansible-playbook -i inventory test.yml'
+sh 'ansible-playbook -i /var/lib/jenkins/workspace/jenkins-ansible@libs/28a61761e665d912145acf05efa14781f0e48dcb5c560d14a767607c80d7d0c0/kibana/tests/inventory /var/lib/jenkins/workspace/jenkins-ansible@libs/28a61761e665d912145acf05efa14781f0e48dcb5c560d14a767607c80d7d0c0/kibana/tests/test.yml'
 }
